88a570e9c7c0e8053e20520a68b4ca269f73ef03
Case Duckworth <acdw@acdw.net>
2022-02-27 21:02:33 -0600

    Backport org-up-heading-or-point-min for older Emacsen

--- org-taskwise.el
+++ org-taskwise.el
@@ -4,7 +4,7 @@
 
 ;; Author: Case Duckworth <acdw@acdw.net>
 ;; Version: 0.1.0
-;; Package-Requires: ((emacs "27.2"))
+;; Package-Requires: ((emacs "24.1"))
 ;; URL: https://github.com/duckwork/org-taskwise.el
 
 ;;; Commentary:
@@ -22,11 +22,25 @@
 
 (require 'org)
 
+;; For older Emacsen than 27.1
+(defun org-taskwise--org-up-heading-or-point-min ()
+  "Move to the heading line of which the present is a subheading, or point-min.
+This version is needed to make point-min behave like a virtual
+heading of level 0 for property-inheritance.  It will return the
+level of the headline found (down to 0) or nil if already at a
+point before the first headline or at point-min."
+  (when (ignore-errors (org-back-to-heading t))
+    (if (< 1 (funcall outline-level))
+        (or (org-up-heading-safe)
+            ;; The first heading may not be level 1 heading.
+            (goto-char (point-min)))
+      (unless (= (point) (point-min)) (goto-char (point-min))))))
+
 (defun org-taskwise-upto-task ()
   "Go to the heading of the current task."
   (interactive)
   (while (not (org-get-todo-state))
-    (org-up-heading-or-point-min)))
+    (org-taskwise--org-up-heading-or-point-min)))
 
 (defun org-taskwise-narrow-to-task ()
   "Narrow the buffer to current task."
